aefa1e36165a1d0ddc8924a8e30ab1f46245745c
zypherscript <kitti.kthp@gmail.com>
2023-05-20 17:39:58 +0700

    up

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+  import Canvas from './lib/Canvas.svelte'
+  import TailwindCss from './TailwindCSS.svelte'
+</script>
+<TailwindCss />
+
+<main>
+  <div class="container mx-auto">
+    <Canvas />
+  </div>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  plugins: [],
+    theme: {
+    extend: {},
+  },
+  purge: ["./index.html",'./src/**/*.{svelte,js,ts}'], // for unused CSS
+  variants: {
+    extend: {},
+  },
+  darkMode: false, // or 'media' or 'class'
+}
+
