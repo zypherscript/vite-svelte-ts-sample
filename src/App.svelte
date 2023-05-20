0cf74cd57d464996da297358c10a0a84fd1dc054
zypherscript <kitti.kthp@gmail.com>
2023-05-21 02:17:38 +0700

    idk

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import Counter from './lib/Counter.svelte'
-  import Canvas from './lib/Canvas.svelte'
+  import Canvas, { type customRgb } from './lib/Canvas.svelte'
   import TailwindCss from './TailwindCSS.svelte'
+
+  const rgb: customRgb = {r: 128, g: 128, b: 128};
 </script>
 <TailwindCss />
 
 <main>
   <div class="container mx-auto">
-    <Canvas />
+    <Canvas mask="/src/assets/svelte-logo-mask.svg" rgb={rgb}/>
+    <!-- <Canvas mask="/src/assets/svelte-logo-mask.svg"/> -->
   </div>
 </main>
--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -1,10 +1,24 @@
-<script>
+<script context="module" lang="ts">
+	export type customRgb = {r: number, g: number, b: number};
+</script>
+
+<script lang="ts">
 	import { onMount } from 'svelte';
+
+	export let mask: string;
+	export let rgb: customRgb;
 
 	let canvas;
 
 	onMount(() => {
-		const ctx = canvas.getContext('2d');
+		canvas.style.webkitMaskImage = `url(${mask})`;
+    	canvas.style.maskImage = `url(${mask})`;
+		canvas.style.webkitMaskPosition = 'center';
+		canvas.style.maskPosition = 'center';
+		canvas.style.webkitMaskRepeat = 'no-repeat';
+		canvas.style.maskRepeat = 'no-repeat';
+
+		const ctx = canvas.getContext('2d', { willReadFrequently: true });
 		let frame;
 
 		(function loop() {
@@ -19,13 +33,12 @@
 
 				const t = window.performance.now();
 
-				const r = 64 + (128 * x / canvas.width) + (64 * Math.sin(t / 1000));
-				const g = 64 + (128 * y / canvas.height) + (64 * Math.cos(t / 1400));
-				const b = 128;
+				const r = 64 + (rgb.r * x / canvas.width) + (64 * Math.sin(t / 1000));
+				const g = 64 + (rgb.g * y / canvas.height) + (64 * Math.cos(t / 1400));
 
 				imageData.data[p + 0] = r;
 				imageData.data[p + 1] = g;
-				imageData.data[p + 2] = b;
+				imageData.data[p + 2] = rgb.b;
 				imageData.data[p + 3] = 255;
 			}
 
@@ -47,7 +60,5 @@
 		width: 100%;
 		height: 100%;
 		background-color: #666;
-		-webkit-mask: url(/src/assets/svelte-logo-mask.svg) 50% 50% no-repeat;
-		mask: url(/src/assets/svelte-logo-mask.svg) 50% 50% no-repeat;
 	}
 </style>
